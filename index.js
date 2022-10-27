7a7d0a56613bc96f8ae6cde2a9a3249aba0e4d2a
hzhang20902 <85701939+hzhang20902@users.noreply.github.com>
2022-10-27 01:53:19 -0400

    changed cors origins

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const nodemailer= require("nodemailer");
 const port = process.env.PORT || 5000
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: "*"
+}));
 app.use(express.json());
 app.use("/api", router);
 app.listen(port, () => console.log("Server Running"));
