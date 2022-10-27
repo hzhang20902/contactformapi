ed6eb76bba0ce072503e84ba1ef955ee61fc82b0
hzhang20902 <85701939+hzhang20902@users.noreply.github.com>
2022-10-27 01:33:11 -0400

    idk lets just try it

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const router = express.Router();
 const cors = require("cors");
 const nodemailer= require("nodemailer");
 
+const port = process.env.PORT || 5000
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api", router);
-app.listen(process.env.PORT || 5000, () => console.log("Server Running"));
+app.listen(port, () => console.log("Server Running"));
 
 const contactEmail = nodemailer.createTransport({
     service: 'gmail',
